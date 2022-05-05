786936b4caba7348e8dede0184f853a0400e0a24
Muzammil Ayaz <muzammilayaz.kabeer@e-consystems.com>
2022-05-05 22:51:27 +0530

    Basic Data API added

--- Basic-Website/src/Components/Data/Data.js
+++ Basic-Website/src/Components/Data/Data.js
@@ -6,13 +6,26 @@ import { Container, containerClasses } from "@mui/material";
 import "./Data.css";
 export default function Data() {
   const navigate = useNavigate();
+  const [api, setApi] = useState(null);
+
+  useEffect(() => {
+    console.log("Api:", api);
+  }, [api]);
+
   return (
     <div className="data-api-container">
-      <div className="api-bar">
+      <div className={api ? "api-bar-up" : "api-bar-down"}>
         <FormControl required>
-          <Select displayEmpty autoWidth>
-            <MenuItem value="" disabled>
-              Barcode Configurations
+          <Select
+            displayEmpty
+            autoWidth
+            value={api}
+            onChange={(e) => {
+              setApi(e.target.value);
+            }}
+          >
+            <MenuItem value={null} disabled>
+              Select an API
             </MenuItem>
             <MenuItem value="1">Get Measure By ID Range</MenuItem>
             <MenuItem value="2">Get Measure By Date Range</MenuItem>
